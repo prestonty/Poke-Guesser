456bd226eb40274fb5f6ee5ea01c5b73a6c4e34e
Preston Tom-Ying <preston.tomying@gmail.com>
2023-10-21 00:51:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Home from "./pages/home.js";
 import Guesser from "./pages/guesser.js";
 import Gallery from "./pages/gallery.js";
 
+import { retrieveFromDatabase } from "./firebase.js";
+
 function App() {
     const HOME = 1;
     const GUESSER = 2;
     const GALLERY = 3;
     var page = GALLERY;
 
-    if (page == HOME) {
+    // WHAT THE HELL, THEY THIK RETRIEVEFROMDATABASE IS A FUNCTION BTU DONT ADD () AFTER???
+    retrieveFromDatabase();
+
+    if (page === HOME) {
         return <Home />;
-    } else if (page == GUESSER) {
+    } else if (page === GUESSER) {
         return <Guesser />;
-    } else if (page == GALLERY) {
+    } else if (page === GALLERY) {
         return <Gallery />;
     }
 }
